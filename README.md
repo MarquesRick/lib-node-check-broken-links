db364a79b1a460322d442fe40d47342f4ccf20ef
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-21 14:48:52 -0300

    use chalk and exemple for using

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ```bash
 # create package.json
 npm init -y
+
+# install chalk - use this version for to use require('')
+npm i chalk@4.1.2
 ```
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Iniciando..");
+const chalk = require("chalk");
+const log = console.log;
+
+// uso de template strings e placeholders
+log(`
+CPU: ${chalk.red("90%")}
+RAM: ${chalk.green("40%")}
+DISK: ${chalk.yellow("70%")}
+`);

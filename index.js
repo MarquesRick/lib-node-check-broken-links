a0e89c9435107286e19c40e08730fa3633e8b6eb
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-22 21:51:13 -0300

    errormessage

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const chalk = require('chalk');
 const fs = require('fs');
 const log = console.log;
 
+const errorHandling = (error) => {
+  throw new Error(chalk.red(error.code, 'No file in path!'));
+};
+
 const getFile = (pathFile) => {
-  fs.readFile(pathFile, (encoding = 'utf-8'), (_, text) => {
+  fs.readFile(pathFile, (encoding = 'utf-8'), (error, text) => {
+    if (error) errorHandling(error);
     log(chalk.green(text));
   });
 };

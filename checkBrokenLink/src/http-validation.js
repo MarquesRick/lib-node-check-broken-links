05275ae190b5c1b60b76aa23b0b880ba0fd39cac
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-27 20:57:11 -0300

    create function generateArrayUrl

--- checkBrokenLink/src/http-validation.js
+++ checkBrokenLink/src/http-validation.js
@@ -1,5 +1,10 @@
+const generateArrayUrl = (arrayLinks) => {
+  arrayLinks = JSON.parse(arrayLinks);
+  return arrayLinks.map((obj) => Object.values(obj).join());
+};
+
 const validateArrayUrl = (arrayLinks) => {
-  return arrayLinks;
+  return generateArrayUrl(arrayLinks);
 };
 
 module.exports = validateArrayUrl;

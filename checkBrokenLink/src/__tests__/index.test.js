8a1410750f008ce2b7bafd830e62c4d369d3a3c4
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-29 20:47:10 -0300

    create and config tests

--- checkBrokenLink/src/__tests__/index.test.js
+++ checkBrokenLink/src/__tests__/index.test.js
@@ -1,5 +1,19 @@
 const getFileAsync = require('../index');
 
-test('get file', () => {
-  expect(typeof getFileAsync).toBe('function');
+const arrayMock = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList',
+  },
+];
+
+describe('getFileAsync::', () => {
+  it('must return array result', async () => {
+    expect(
+      JSON.parse(
+        await getFileAsync(
+          '/Users/hmarques/work/Cursos/Alura/nodejs/node-lib-checkBrokenLink/checkBrokenLink/src/__tests__/files/text.md'
+        )
+      )
+    ).toEqual(arrayMock);
+  });
 });

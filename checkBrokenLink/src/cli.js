4106035343e25e305b282f76235fd2561880b454
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-29 18:47:38 -0300

    create check urls

--- checkBrokenLink/src/cli.js
+++ checkBrokenLink/src/cli.js
@@ -7,7 +7,10 @@ const path = process.argv;
 const processText = async (pathFile) => {
   const result = await getFileAsync(pathFile[2]);
   if (path[3] === 'validar')
-    console.log(chalk.yellow('links validados'), validateArrayUrl(result));
+    console.log(
+      chalk.yellow('links validados'),
+      await validateArrayUrl(result)
+    );
   else console.log(chalk.yellow('lista de links'), result);
 };
 
--- checkBrokenLink/src/http-validation.js
+++ checkBrokenLink/src/http-validation.js
@@ -1,10 +1,25 @@
+const fetch = require('node-fetch');
+
+const checkStatus = async (arrayUrl) => {
+  const arrayStatus = await Promise.all(
+    arrayUrl.map(async (url) => {
+      const res = await fetch(url);
+      return res.status;
+    })
+  );
+
+  return arrayStatus;
+};
+
 const generateArrayUrl = (arrayLinks) => {
   arrayLinks = JSON.parse(arrayLinks);
   return arrayLinks.map((obj) => Object.values(obj).join());
 };
 
-const validateArrayUrl = (arrayLinks) => {
-  return generateArrayUrl(arrayLinks);
+const validateArrayUrl = async (arrayLinks) => {
+  const links = generateArrayUrl(arrayLinks);
+  const statusLinks = await checkStatus(links);
+  return statusLinks;
 };
 
 module.exports = validateArrayUrl;
